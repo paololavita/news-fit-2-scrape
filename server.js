5cabe45744aba34c76eeba70db2d7a6b1430b882
Jonas Grumby <plavita@hotmail.com>
2018-09-05 14:36:41 -0400

    2nd Update

--- server.js
+++ server.js
@@ -26,8 +26,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/webscraper";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/week18Populater");
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+
+// Connect to the Mongo DB
+//mongoose.connect("mongodb://localhost/webscraper");
 
 // Routes
 
