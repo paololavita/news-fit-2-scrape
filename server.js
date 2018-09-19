c1011e1928b3edb3caa3dc2caca3db87ee0c7f9a
Jonas Grumby <plavita@hotmail.com>
2018-09-18 21:10:38 -0400

    Fixed Bug

--- server.js
+++ server.js
@@ -23,14 +23,14 @@ var app = express();
 app.use(logger("dev"));
 
 // Use body-parser for handling form submissions
-//app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ useNewUrlParser: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ useNewUrlParser: true }));
 
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/webscraper";
+var MONGODB_URI = process.env.MONGODB_URI || ("mongodb://localhost/webscraper", { useNewUrlParser: true });
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
