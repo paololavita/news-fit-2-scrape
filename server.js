8cf170814129dad33224bfd726314ced060e5fef
Jonas Grumby <plavita@hotmail.com>
2018-09-18 21:43:20 -0400

    Fixed Bug

--- server.js
+++ server.js
@@ -30,12 +30,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || ("mongodb://localhost/webscraper", { useNewUrlParser: true });
+var CONNECTION_URI = process.env.MONGODB_URI || "mongodb://localhost/webscraper";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect(CONNECTION_URI, {useMongoClient: true});
 
 
 // Connect to the Mongo DB
