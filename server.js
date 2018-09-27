121a08141432fb49d22d627fe4426dad85c75bfc
Jonas Grumby <paololavita@inside-65-59-167.wireless.utoronto.ca>
2018-09-26 21:48:03 -0400

    Changed

--- server.js
+++ server.js
@@ -30,12 +30,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/webscraper";
-var MONGODB_URI = "mongodb://heroku_rp0f279v:fisu5g52gs7lbvrj2v3jkcttqe@ds261302.mlab.com:61302/heroku_rp0f279v" || "mongodb://localhost/webscraper";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/webscraper";
+//var MONGODB_URI = "mongodb://heroku_rp0f279v:fisu5g52gs7lbvrj2v3jkcttqe@ds261302.mlab.com:61302/heroku_rp0f279v" || "mongodb://localhost/webscraper";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-//mongoose.Promise = Promise;
+mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
 
